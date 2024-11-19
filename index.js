3ee93f1263cbe0e1a8f9b32c95f109c694ff1a76
klausqama <klaussqama@gmail.com>
2024-11-19 17:12:32 +0100

    add ex

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const {posts} = require('./posts');
+const app = express();
+const port = 3000
+
+
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('Server del mio blog ZZZZZZZZ')
+});
+
+app.get('/bacheca', (req, res) => {
+    res.json({
+        posts: posts,
+        count: posts.length,
+    })
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
